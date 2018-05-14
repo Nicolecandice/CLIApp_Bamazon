a5ba5d6736e3ee7c36c9ac4cf8e2c00c23413c0b
Nicole Cooper <nicolecooper@Nicoles-MBP.home>
2018-05-14 18:02:25 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,11 +32,12 @@ function queryAllSaleProducts() {
     console.log('Items on sale...\n');
 
     console.log('ID | Product_Name | Price (products on sale)');
-    console.log('---------------------------------------------------------------')
 
+    console.log('--------------------------------------------')
 
-    // loop through database for sale's items
+    // loop through database and display all items.
     for (var i = 0; i < res.length; i++) {
+
       // convert to string
       var item_ID = res[i].itemID + '';
       itemID = (" ID ", item_ID);
@@ -49,80 +50,80 @@ function queryAllSaleProducts() {
 
       // Log table entry
       console.log(item_ID + '|' + name + '|' +  price);
-
     }
+    //----------------------------------------------------------------------------------
+  prompt.start();
+         console.log("\nWhich item(ID) will you like to purchase?");
+         prompt.get(["itemID"]), function (err, res) {
 
-  });
+          // Display item(ID) selected
+          var itemID = res.itemID;
+          console.log("ItemID" + '.');
+          //-------------------------------------------------------------------
 
-}
-  // // The App prompt user this massages
-  // prompt.start();
+         console.log("\nHow many items will you like to purchase?");
+         prompt.get(["itemIDQuantity"]), function (err, res) {
 
-  // // Customer placed order
-  // prompt.get("buy_itemID", function (err, res) {
-
-  //   // Display ID and Quantity selected
-  //   var buy_itemID = res.buy_itemID;
-  //   message('select product ID')
-  //   console.log('Order selected is ' + buy_itemID + '.');
-
-  //   var itemIDQuantity = res.itemID_Quantity;
-  //   message('How many units of the product that you would like to buy ')
-  //   console.log('Order amount selected is ' + '.');
-
-
-    // the application has to check if store has enough product to meet customer request
-  //   connection.query('SELECT stockQuantity FROM products where ?' {
-  //     itemID: buy_itemID
-  //    function (err, res) {
-  //     // Error handler
-  //     if (err) throw (err);
-
-  //     var bamazonQuantity = res.StockQuantity;
-  //     //insufficient quantity
-  //     if (bamazonQuantity < StockQuantity) {
-  //       console.log('Sorry products out of stock' + bamazonQuantity + 'for these items order Cancelled.')
-  //     } else(bamazonQuantity >= StockQunantity)
-  //     // update mysql database with new inventory
-
-  //     var newInventory = parseInt(bamazonQuantity) + parseInt(buyid_item);
-  //     connection.query('UPDATE Products SET? WHERE?', [{
-  //       StockQuantity: newInventory
-  //     }, {
-  //       item_ID: buy_itemID
-  //     }], function (err, res) {
-  //       if (err) throw err;
-  //     });
-
-  //   });
-   });
-
-
-  function updateProducts() {
-    console.log('Updating  products quantities...\n');
-    var query = connection.query(
-      'UPDATE products SET ?', [{
-          quantity: 175
-        },
-        {
-          Product_Name: "dress"
+          // Display number of item(s) selected
+          var itemIDQuantity = res.itemIDQuantity;
+          console.log(" itemIDQuantity  selected is"+ '.');
+         }
         }
-      ],
-      function (err, res) {
-        console.log(res.affectedRows + "products updated!\n");
-        // Call updatedProduct AFTER the UPDATE completes
-        updateProducts();
 
-        // log the query being run
-        console.log(query.sql);
-      });
-  }
+     //the application has to check if store has enough product to meet customer request
+     connection.query('SELECT stockQuantity FROM products WHERE ?' ,[{itemID: itemIDQuantity}], function (err, res) {
+      // Error handler
+     if (err) throw err;
+
+         var bamazonQuantity = res[0].StockQuantity;
+         if(bamazonQuantity <= itemIDQuantity){
+          console.log('Sorry products out of stock' + bamazonQuantity + 'for these items order Cancelled.');
+          connection.end(); // end of scrpit connection
+         }
+    
+      // update mysql database with new inventory
+      else {
+
+        var newInventory = parseInt(bamazonQuantity) + parseInt(itemIDQuantity);
+         connection.query('UPDATE Products SET? WHERE?', [{
+         stockQuantity: newInventory
+       }, {
+         itemID: itemIDQuantity
+        }], function (err, res) {
+        if (err) throw err;
+       });
+      }
+
+    // });
+    // });
+
+
+  // function updateProducts() {
+  //   console.log('Updating  products quantities...\n');
+  //   var query = connection.query(
+  //     'UPDATE products SET ?', [{
+  //         quantity: 175
+  //       },
+  //       {
+  //         Product_Name: "dress"
+  //       }
+  //     ],
+  //     function (err, res) {
+  //       console.log(res.affectedRows + "products updated!\n");
+  //       // Call updatedProduct AFTER the UPDATE completes
+  //       updateProducts();
+
+  //       // log the query being run
+  //       console.log(query.sql);
+  //     });
+  // }
 
   // var cusTotal;
   // connection.query('SELECT Price FROM Products WHERE ?', [{
-  //   itemID: buy_itemID
+  //   itemID: itemIDQuantity
   // }], function (err, res) {
   //   var purchasePrice = result[0].price;
-  //   cusTotal = buyItemQuantity * purchasePrice.tofixed(2);
+  //   cusTotal = itemIDQuantity * purchasePrice.tofixed(2);
   //   console.log('\n Customer total is $' + cusTotal + '.');
-  // });
+   //   });
+    
--- bamazonManager.js
+++ bamazonManager.js
@@ -18,4 +18,95 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   
-});
+//------------------------------------------------------------
+// Prompt user to select meun option.
+prompt.start();
+
+  console.log("Meun");
+  console.log("--------------------")
+  console.log("menu option")
+  console.log ("1. view  Product for sale");
+  console.log("2. View Low Inventory");
+  console.log("3. Add to Inventory");
+  console.log("4. Add New Product");
+  
+  prompt.get("slectedMeun",function (err,res) {
+
+    // Switch case options
+    var selectedMenu = parseInt(res.selectedMenu);
+
+    switch(selectedMenu) {
+      case 1:
+      console.log('\nView Products for sale...')
+      // note thta this function uses a callback
+      viewProductsSale(function() {});
+      //end scrpition/connection
+      connection.end();
+      break;
+
+      case 2:
+      console.log('\nView low inventiory...');
+      wiewLowInventiory();
+      connection.end();
+      break;
+
+      case 3:
+      console.log("Add to inventory...");
+      addInventory();
+      break;
+
+      case 4:
+      console.log("Add New Product");
+      addNewProduct();
+      break;
+    }
+
+  });
+
+  });
+  
+  //===================Function for the switch case=========================================
+
+  // Call back function for the View Products for Sale
+  function viewProductsSale(callback) {
+
+    connection.query("SELECT * FROM Products", function(err,res) {})
+    
+    if(err) throw err;
+
+    console.log("Total Inventory...\n");
+    
+    // Table Header
+    console.log("ID    |    Product Name    |    Department Name   | Price   | In Stock");
+    console.log("----------------------------------------------------------------------")
+
+     // loop through database and display all items.
+     for (var i = 0; i < res.length; i++) {
+
+      // convert to string
+      var item_ID = res[i].itemID + '';
+      itemID = (" ID ", item_ID);
+
+      var name = res[i].productName + '';
+      name = (" ProductName ", name);
+
+      var name = res[i].departmentName + '';
+      name = (" departmentName ", name);
+
+      var price = '$' + res[i].price.toFixed(2) + '';
+      price = (" Price (products on sale)", price + '');
+
+      var quqntity = res[i].stockQuantity  + " ";
+
+      // Log table entry
+      console.log(item_ID + '|' + productname + '|' + departmentName + '|' + price  + '|'  + quqntity);
+     }
+      callback();
+  
+    
+
+
+
+  
+
+
